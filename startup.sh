9f7c4e6dd2e36c571324fed9228e7b09f35efe19
d0mb1 <114655039+d0mb1@users.noreply.github.com>
2022-12-12 15:14:17 +0100

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off
+feh --bg-scale ~/Pictures/mech.jpg
+
+nm-applet
+cbatticon
+
+# -- Autostart
+# awful.spawn.with_shell("~/.config/awesome/startup.sh")
